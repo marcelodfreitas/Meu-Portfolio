32842f76afdfb230b5b34b01eeaade27f054b9f6
Marcelo Freitas <markfreitaas@outlook.com>
2024-07-30 16:53:33 -0300

    atualização do portfolio com dados pessoais

---
+++ scripts.js
@@ -0,0 +1,70 @@
+
+/* toggle icon navbar */
+
+let menuIcon = document.getElementById("menuIcon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = function showHide() {
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
+
+/* SCROLL SECTION ACTIVE LINK */
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector("header nav a[href=" + id + "]").classList.add("active");
+      });
+    }
+  });
+
+  /* STICKY NAVBAR */
+
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /* REMOVE TOGGLE ICON AND NAVBAR */
+
+  menuIcon.onclick = function showHide() {
+    menuIcon.classList.remove("fa-xmark");
+    navbar.classList.remove("active");
+  };
+
+  /* SCROLL REVEAL */
+
+  ScrollReveal({
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+  });
+
+  ScrollReveal().reveal(".home-content, heading", { origin: "top" });
+  ScrollReveal().reveal(
+    ".home-img, .services-container, .portfolio-box, form",
+    { origin: "button" }
+  );
+  ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+  ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+  /* TYPED JS */
+
+  
+  const typed = new Typed("#text-multiple", {
+    strings: ["FullStack Developer and NoCode","Web Designer","Musician",],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+  });
+};
